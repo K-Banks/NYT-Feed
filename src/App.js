e026ee5e1ee856c52f2193533113cbaa758f9f6e
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-20 11:57:20 -0700

    Set up basic files for API query and state management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { newState } from './constants';
 
 function App() {
+
+  console.log(newState.name);
+
   return (
     <div className="App">
       <header className="App-header">
