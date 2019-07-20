4a973ca0daaf3b8d3314bb983a877e5080dcaed3
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-20 15:20:09 -0700

    Modify API call to store articles in state object

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    let articles = {};
     let freshState = Object.assign({}, newState);
     this.state = freshState;
     this.getArticles = this.getArticles.bind(this);
+    this.handleResponseError = this.handleResponseError.bind(this);
+    this.populateArticles = this.populateArticles.bind(this);
+    this.getArticles();
+
   }
 
   getArticles() {
@@ -19,22 +24,43 @@ class App extends React.Component {
         if (response.status === 200) {
           return response.json()
         } else {
-          this.handleResponseError(response)
+          this.handleResponseError(response);
+          return null;
         }
       }
     ).then(
       body => {
-        console.log(body);
+        if (body) {
+          this.state.articles = body.results;
+          console.log(this.state);
+          // this.populateArticles();
+        }
       }
     )
+  }
+
+  populateArticles() {
+    // for (var i = 0; i < this.articles.length; i++) {
+    //   let article = this.articles[i];
+    //   let target = document.getElementsByClassName('feed');
+    //   let newArticle = <Article props={article} />;
+    //   target.appendChild(newArticle);
+    // }
+  }
+
+  handleResponseError(response) {
+    console.log('There was an error: Response ' + response.status);
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-      <h1>NYT Feed</h1>
-      </header>
+        <header className="App-header">
+          <h1>NYT Feed</h1>
+        </header>
+        <div className="feed">
+
+        </div>
       </div>
     );
   }
