b7da6a830e07d8b6f1fa84fcd1f3e20564985557
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-20 13:50:14 -0700

    add example html using prop keys

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -2,5 +2,13 @@ import React from 'react';
 import './article.css';
 
 function Article(props){
-  
+  return(
+    <div>
+      <h1><a href={props.url}>{props.title}</a></h1>
+      <h2>{props.byline}</h2>
+      <img src={props.thumbnail_standard} alt="article image"></img>
+    </div>
+  );
 }
+
+export default Article;
