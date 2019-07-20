ffbd019c0e84834ea63ace350d5ec7b67f0026d8
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-20 12:55:56 -0700

    Add short testing of livefeed endpoint and log responses

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import { newState } from './constants';
 
 function App() {
 
+  console.log(process.env.REACT_APP_CLIENT_ID);
+
+  const url = 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=' + process.env.REACT_APP_CLIENT_ID;
+  fetch(url).then(
+    response => {
+      if (response.status === 200) {
+        return response.json()
+      } else {
+        this.handleResponseError(response)
+      }
+    }
+  ).then(
+    body => {
+      console.log(body);
+    }
+  )
+
   return (
     <div className="App">
       <header className="App-header">
