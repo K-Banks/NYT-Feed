ec7282bcc1cb61f57b2c7eba4d8fbe593f611a60
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-21 11:45:33 -0700

    Update structure of sectionsConstant

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,113 @@ export const newState = {
   queryTimeOut: 0,
 };
 
-export const sectionsConstant = {
-  sectionsArray: ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'national', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 'tmagazine', 'travel', 'upshot', 'world'],
-  sectionsTitle: ['Arts', 'Automobiles', 'Books', 'Business', 'Fashion', 'Food', 'Health', 'Home', 'Insider', 'Magazine', 'Movies', 'National', 'New York Region', 'Obituaries', 'Opinion', 'Politics', 'Real Estate', 'Science', 'Sports', 'Sunday Review', 'Technology', 'Theater', 'Time Magazine', 'Travel', 'Upshot', 'World']
-};
+export const sectionsConstant = [
+  {
+    title: 'All',
+    value: null
+  },
+  {
+    title: 'Arts',
+    value: 'arts'
+  },
+  {
+    title: 'Automobiles',
+    value: 'automobiles'
+  },
+  {
+    title: 'Books',
+    value: 'books'
+  },
+  {
+    title: 'Business',
+    value: 'business'
+  },
+  {
+    title: 'Fashion',
+    value: 'fashion'
+  },
+  {
+    title: 'Food',
+    value: 'food'
+  },
+  {
+    title: 'Health',
+    value: 'health'
+  },
+  {
+    title: 'Home',
+    value: 'home'
+  },
+  {
+    title: 'Insider',
+    value: 'insider'
+  },
+  {
+    title: 'Magazine',
+    value: 'magazine'
+  },
+  {
+    title: 'Movies',
+    value: 'movies'
+  },
+  {
+    title: 'National',
+    value: 'national'
+  },
+  {
+    title: 'New York Region',
+    value: 'nyregion'
+  },
+  {
+    title: 'Obituaries',
+    value: 'obituaries'
+  },
+  {
+    title: 'Opinion',
+    value: 'opinion'
+  },
+  {
+    title: 'Politics',
+    value: 'politics'
+  },
+  {
+    title: 'Real Estate',
+    value: 'realestate'
+  },
+  {
+    title: 'Science',
+    value: 'science'
+  },
+  {
+    title: 'Sports',
+    value: 'sports'
+  },
+  {
+    title: 'Sunday Review',
+    value: 'sundayreview'
+  },
+  {
+    title: 'Technology',
+    value: 'technology'
+  },
+  {
+    title: 'Theater',
+    value: 'theater'
+  },
+  {
+    title: 'Time Magazine',
+    value: 'tmagazine'
+  },
+  {
+    title: 'Travel',
+    value: 'travel'
+  },
+  {
+    title: 'Upshot',
+    value: 'upshot'
+  },
+  {
+    title: 'World',
+    value: 'world'
+  }
+];
