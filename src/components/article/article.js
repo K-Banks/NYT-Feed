b96415414b3202231f7ec629b3bf8b10410be21d
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-20 17:16:33 -0700

    Correctly grab information from props passed into component

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './article.css';
 
 function Article(props){
-  return(
-    <div>
-      <h1><a href={props.url}>{props.title}</a></h1>
-      <h2>{props.byline}</h2>
-      <img src={props.thumbnail_standard} alt="article image"></img>
-    </div>
-  );
+
+  if (props.props) {
+    return(
+      <div>
+        <h1><a href={props.props.url}>{props.props.title}</a></h1>
+        <h2>{props.props.byline}</h2>
+        <img src={props.props.thumbnail_standard} alt="article"></img>
+      </div>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default Article;
