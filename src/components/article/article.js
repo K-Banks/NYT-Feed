3c043ab1ca392f8e6cdc9ae6bdf186f0d8d3fdb6
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-21 14:16:16 -0700

    Update image render with a conditional statement to handle missing image files gracefully

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -8,7 +8,12 @@ function Article(props){
       <div className="article-container">
         <div></div>
         <div className="image">
-          <img src={props.props.thumbnail_standard} alt="article"></img>
+
+        {props.props.multimedia[0] ? (
+          <img src={props.props.multimedia[0].url} alt="article"></img>
+        ) : (
+          <img src={require('../../assets/nyt-logo.png')} alt="article"></img>
+        )}
         </div>
         <div className="text">
           <h3><a href={props.props.url}>{props.props.title}</a></h3>
