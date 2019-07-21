34d0537880a3bf87ef640035c633023ae5185abd
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-21 13:17:15 -0700

    Add identification of selected section upon click

--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -6,11 +6,15 @@ function Toolbar(props){
     let sections = props.props;
     let menuOptions = [];
     sections.forEach((section) =>
-      menuOptions.push(<li key={section.value}>{section.title}</li>));
+      menuOptions.push(<li key={section.value} onClick={() => {getSelectedSection(section)}}>{section.title}</li>));
+
+    function getSelectedSection(section) {
+      props.changeSelectedSection(section)
+    }
 
     return (
       <div  className="dropdown" >
-      <div className="button" onClick={props.showDropdownMenu}> Select Section </div>
+      <div className="button" onClick={props.showDropdownMenu}> {props.state.selectedSection.title} </div>
 
         { props.state.displayMenu ? (
           <ul>
