64c979beb9be2b849d6d848f082426f2553ea89d
Kayl Eubanks <kayleubanks@gmail.com>
2019-07-21 17:11:32 -0700

    Updates

---
+++ precache-manifest.75c4ac86d386e15959d65123a6147508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef653144004fcc9159ea233edcfd7f2",
+    "url": "/NYT-Feed/index.html"
+  },
+  {
+    "revision": "581f322266c2f1b5f423",
+    "url": "/NYT-Feed/static/css/main.66ba2602.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/NYT-Feed/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "581f322266c2f1b5f423",
+    "url": "/NYT-Feed/static/js/main.2605f15f.chunk.js"
+  },
+  {
+    "revision": "f16de87c0e58500bc1df",
+    "url": "/NYT-Feed/static/js/runtime~main.adea4cc5.js"
+  },
+  {
+    "revision": "8662bc4e3cf7980bda20a673b43c3ca2",
+    "url": "/NYT-Feed/static/media/nyt-logo.8662bc4e.png"
+  }
+]);
